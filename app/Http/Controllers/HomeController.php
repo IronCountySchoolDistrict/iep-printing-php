b31c7d494d7a1ea2fb0111e9c719f0ca4828e0e0
wallyjaw <wallyjaw@gmail.com>
2015-06-01 13:29:03 -0600

    added TODO comments with suggestions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,41 +11,34 @@ class HomeController extends Controller {
 
     public function index()
     {
-        // $pdf = new Pdf(storage_path() . DIRECTORY_SEPARATOR . 'forms' . DIRECTORY_SEPARATOR . 'SpEd 05-1 pg1-fillable.pdf');
-
-        // $values = [
-        //     "Attending School" => "Value",
-        //     "Classification" => "Value",
-        //     "DOB" => "Value",
-        //     "Eligibility Date" => "Value",
-        //     "Grade" => "Value",
-        //     "IEP Meeting" => "Value",
-        //     "Next ReEvaluation" => "Value",
-        //     "PLAAFP" => "ValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValueValue",
-        //     "Primary Server" => "Value",
-        //     "Student" => "Value",
-        // ];
-
-        // $pdf->fillForm($values)
-        //     ->flatten()
-        //     ->needAppearances()
-        //     ->saveAs(storage_path() . DIRECTORY_SEPARATOR . 'forms' . DIRECTORY_SEPARATOR . 'filled.pdf');
-
-        // dd($pdf);
-
         if (Request::isMethod('get')) {
             return 'Hi!';
         } else if (Request::isMethod('post')) {
-            $data = json_decode(Request::input('responses'));
+            $responses = json_decode(Request::input('responses'));
 
-            $pdfFile = storage_path() . DIRECTORY_SEPARATOR . 'forms' . DIRECTORY_SEPARATOR . $data->form->title . '.pdf';
+            $pdfFiles = [];
+            foreach ($responses as $response) {
+                $file = storage_path() . DIRECTORY_SEPARATOR . 'forms' . DIRECTORY_SEPARATOR . $response->form->title . '.pdf';
+                if (file_exists($file)) {
+                    $pdf = new Pdf($file);
 
-            if (file_exists($pdfFile)) {
-                $pdf = new Pdf($pdfFile);
+                    $fillForm = [];
+                    foreach ($response->response as $formFields) {
+                        $fillForm[$formFields->title] = $formFields->response;
+                    }
+
+                    $pdf->fillForm($fillForm)
+                        ->flatten()
+                        ->needAppearances()
+                        ->saveAs(); // TODO: save as what? can we get the temp location?
+                }
             }
 
+            // TODO: zip if $pdfFiles length > 1
 
-            return Response::json($pdf);
+            // TODO: move files to a location where we can give a link to it (not temp_dir)
+
+            return Response::json($pdf->getDataFields());
         }
     }
 
