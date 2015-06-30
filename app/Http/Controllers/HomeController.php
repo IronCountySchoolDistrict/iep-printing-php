9074942d4072070062c82a137bbb93614670defe
wallyjaw <wallyjaw@gmail.com>
2015-06-30 12:04:23 -0600

    added command logic for getting a list of printable blank forms and changed the way routing works with ajax post calls with different types of requests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Iep\Student;
 use App\Commands\FillPdfCommand;
 use App\Commands\AssemblePdfCommand;
 use App\Http\Controllers\Controller;
+use App\Commands\GetBlankPdfListCommand;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 
 class HomeController extends Controller {
@@ -15,39 +16,46 @@ class HomeController extends Controller {
     public function index()
     {
         if (Request::isMethod('get')) {
-            $forms = json_decode('[{"id":"316866","title":"IEP: SpEd 05-1 v.150501"}]');
-            $action = 'blank';
-            $student = json_decode('{"lastfirst":"Fabela, Phoebe Arian","first_name":"Phoebe","middle_name":"Arian","last_name":"Fabela","student_number":60013447,"entrydate":"08/14/2014","exitdate":"05/23/2015","gender":"F","current_school":"South Elementary School","dob":"06/11/2008","street":"375 S 1000 W","city":"Cedar City","state":"UT","zip":"84720","next_school":"South Elementary School","enrollment_school":"South Elementary School","father":"","mother":"Behunin, Jennifer","grade":1}');
-            $responses = json_decode('[{"form":{"id":316866,"title":"IEP: SpEd 05-1 v.150501","description":"PLAFFP","type":"P"},"response":[{"field":"iep-meeting","type":"text","response":"05/05/2015"},{"field":"classification","type":"dropdown","response":"Hearing Impairment/Deafness"},{"field":"plaafp","type":"paragraph","response":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non metus sit amet augue consequat lacinia faucibus at dolor. Aenean efficitur, arcu a iaculis blandit, tortor nisi cursus metus, vel accumsan nisl odio in ante. Fusce lacus augue, bibendum sed maximus interdum, ullamcorper sed urna. Morbi vel nulla arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed tristique, augue quis pulvinar suscipit, purus justo sodales orci, efficitur porttitor libero odio et mauris. Nunc finibus, ligula a posuere egestas, nibh urna viverra justo, vel tristique eros ex et neque. Nunc facilisis velit ac elementum tristique. Ut sed ornare ex, non aliquam nibh. Ut vel fringilla eros. Nam mi enim, tempus vel lacus quis, ornare imperdiet nisl.Curabitur scelerisque, velit non faucibus eleifend, diam orci posuere sem, ut mattis nulla dui eu mauris. Nam orci diam, condimentum at eros eget, placerat lacinia mi. Vestibulum tortor sapien, pulvinar ut diam vel, sagittis consectetur neque. Suspendisse cursus massa vitae nisi ornare viverra. Nullam pulvinar lacus venenatis dapibus maximus. Morbi sollicitudin tempor elit sed tempus. Nam dolor erat, malesuada vel est eu, dictum malesuada nisi. Cras fermentum auctor tellus at rhoncus. Phasellus vel luctus tellus. Praesent sollicitudin augue et felis interdum, nec imperdiet elit facilisis. Maecenas tristique quis est eget rhoncus. Vivamus interdum ipsum sit amet sapien rutrum mollis. Nullam interdum lectus id libero sagittis hendrerit. Donec libero ex, cursus eget ex vitae, posuere pellentesque urna."},{"field":"grade","type":"dropdown","response":"3"},{"field":"eligibility-date","type":"text","response":"05/01/2015"},{"field":"attending-school","type":"dropdown","response":"East Elementary"},{"field":"next-re-evaluation","type":"text","response":"05/01/2016"},{"field":"primary-server","type":"text","response":"Mr. Alibaba"}]},{"form":{"id":396736,"title":"IEP: SpEd 7b","description":"Written Prior Notice and Consent for Change of Placement in Special Education","type":"P"},"response":[{"field":"placement-effective-date","type":"text","response":"01/02/2003"},{"field":"placement","type":"checkbox","response":"Regular Class with part-time and/or itinerant special education services|1, Special Class|2, Special school|3, Home instruction|4, Hospital/Institutional|5, No longer eligible for services due to Graduation with a high school diploma|6, No longer eligible for services due to Reaching maximum age of eligibility, 22 years|7"},{"field":"this-option-was-selected","type":"radio","response":"OtherUtherAtherEtherItherYther"},{"field":"date","type":"hidden","response":"6/16/15"},{"field":"upon-exiting-the-lea","type":"checkbox","response":"Yes"},{"field":"attach-copy","type":"checkbox","response":"Yes, No"}]}]');
+            $forms = json_decode('[{"form_id":316100,"title":"IEP: SpEd Preschool v.150501","type":"P","desc":"Referral for Evaluation for Special Education Services Preschool"},{"form_id":316866,"title":"IEP: SpEd 05-1 v.150501","type":"P","desc":"PLAFFP"},{"form_id":319384,"title":"IEP: SpEd 11","type":"P","desc":"Notice to Parents and Students Regarding Age of Majority Rights That Transfer under IDEA"},{"form_id":319561,"title":"IEP: SpEd 6a1","type":"P","desc":"Individualized Education Program"},{"form_id":394525,"title":"IEP: SpEd 8","type":"P","desc":"Written Prior Notice of Evaluation/Re-Evaluation and Review of Existing Data"},{"form_id":396527,"title":"IEP: SpEd 05E v.150501","type":"P","desc":"Statement Regarding Participation in Extracurricular Activities"},{"form_id":397199,"title":"IEP: SpEd 12 v.150501","type":"P","desc":"Acknowledgement for Placement in Home Schooling"},{"form_id":401957,"title":"IEP: SpEd 4","type":"P","desc":"Notice of Meeting"},{"form_id":402659,"title":"IEP: SpEd 13","type":"P","desc":"Academic Observation Report"},{"form_id":403303,"title":"IEP: Sped 19","type":"P","desc":"Authorization for Release and Use of Health Information"},{"form_id":-432079,"title":"grade level archive","type":"P","desc":""},{"form_id":-316023,"title":"IEP: SpEd 21 v.150501","type":"P","desc":"Summary of Academic Achievement and Functional Performance"},{"form_id":319275,"title":"IEP: SpEd 6e","type":"P","desc":"Individualized Transition Plan"},{"form_id":396671,"title":"IEP: SpEd 7a","type":"P","desc":"Written Prior Notice and Consent for Initial Placement in Special Education"},{"form_id":397092,"title":"IEP: SpEd 11 v.150501","type":"P","desc":"Individualized Education Plan Summary"},{"form_id":401857,"title":"IEP: SpEd 6h","type":"P","desc":"Amendment to IEP"},{"form_id":402585,"title":"IEP: SpEd 24","type":"P","desc":"Authorization to Access Confidential Student Records"},{"form_id":403748,"title":"IEP: SpEd 9","type":"P","desc":"Written Prior Notice of Refusal to take Action"},{"form_id":410056,"title":"Iron 6-12 Computer Acceptable Use Policy- Student","type":"P","desc":"Computer Acceptable Use Policy"},{"form_id":-316502,"title":"IEP: SpEd 05 v.150501","type":"P","desc":"Individualized Education Program"},{"form_id":317013,"title":"IEP: SpEd 6c","type":"P","desc":"Measurable Annual Goal(s) and Report of Progress"},{"form_id":317658,"title":"IEP: SpEd 6f1","type":"P","desc":"Participation in Assessment Programs"},{"form_id":320290,"title":"IEP: SpEd 01 v. 150501","type":"P","desc":"Student Intervention Profile  Tier I, II and III"},{"form_id":-394910,"title":"IEP: SpEd 05d v.150501","type":"P","desc":"Participation in Assessment Programs"},{"form_id":396736,"title":"IEP: SpEd 7b","type":"P","desc":"Written Prior Notice and Consent for Change of Placement in Special Education"},{"form_id":396809,"title":"IEP: SpEd 10 v.150501","type":"P","desc":"Individualized Services Plan"},{"form_id":397256,"title":"IEP: SpEd 13 LD1 v.150501","type":"P","desc":"Learning Disabilities Observation Report"},{"form_id":397323,"title":"IEP: SpEd 15","type":"P","desc":"Consent for Agency Invitation to Transition Meeting"},{"form_id":397664,"title":"IEP: SpEd 20 v.150501","type":"P","desc":"Medicaid Reimbursement Notification"},{"form_id":397709,"title":"IEP: SpEd 34 v.150501","type":"P","desc":"Revocation of Consent for Special Education and Related Services  34 C.F.R SS 300.9(c)(3) and 300.300(b)(4)"},{"form_id":400315,"title":"IEP SpEd 6d 04.08","type":"P","desc":"ESY"},{"form_id":402186,"title":"IEP: SpEd4a","type":"P","desc":"Notice of Meeting for Adult Student"},{"form_id":402966,"title":"IEP: SpEd 12","type":"P","desc":"Consent for Disclosure of Confidential Information"},{"form_id":407322,"title":"Iron K-5 Acceptable Use Policy- Parent","type":"P","desc":"Computer Acceptable Use Policy"},{"form_id":-432071,"title":"grade level archive","type":"S","desc":""},{"form_id":432168,"title":"grade level","type":"S","desc":""},{"form_id":394668,"title":"IEP: SpEd 2a","type":"P","desc":"Referral for Evaluation for Special Education Services"},{"form_id":394835,"title":"IEP: SpEd 03a","type":"P","desc":"Written Prior Notice and Consent for Evaluation/Re-Evaluation"},{"form_id":396630,"title":"IEP: SpEd 05F v.150501","type":"P","desc":"Individualized Education Program (IEP) Addendum"},{"form_id":397382,"title":"IEP: SpEd 6i","type":"P","desc":"Agreement to Excuse IEP Team Member"},{"form_id":397491,"title":"IEP: SpEd 16","type":"P","desc":"Manifestation Determination"},{"form_id":399873,"title":"IEP: SpEd6g ESY","type":"P","desc":"Extended School Year Services-IEP Attachment"},{"form_id":402477,"title":"IEP: SpEd 17","type":"P","desc":"Record of Access"},{"form_id":404001,"title":"IEP: SpEd 1","type":"P","desc":"Regular Education Interventions/At Risk Documentation"},{"form_id":407365,"title":"Iron K-5 Acceptable Use Policy- Student","type":"P","desc":"Computer Acceptable Use Policy"},{"form_id":410549,"title":"Iron 6-12 Computer Acceptable use Policy- Parent","type":"P","desc":"Computer Acceptable Use Policy"},{"form_id":410991,"title":"Iron Safe Schools Policy","type":"P","desc":"Safe Schools Policy JFB"},{"form_id":415827,"title":"IEP: SpEd 12a","type":"P","desc":"Release of Student Records"}]');
+            $action = 'getBlanks';
 
-            if ($action == 'blank') {
-                $info = $this->dispatch(
-                    new AssemblePdfCommand($forms)
-                );
-            } else {
-                $info = $this->dispatch(
-                  new FillPdfCommand($student, $responses)
-                );
-            }
+            $info = $this->dispatch(
+                new GetBlankPdfListCommand($forms)
+            );
 
             return response()->json($info);
             // return 'Hi!';
         } else if (Request::isMethod('post')) {
-            if (Request::input('action') == 'blank') {
-                $forms = json_decode(Request::input('forms'));
-
-                $info = $this->dispatch(new AssemblePdfCommand($forms));
-            } else {
-                $info = $this->dispatch(
-                    new FillPdfCommand(
-                        json_decode(Request::input('student')),
-                        json_decode(Request::input('responses'))
-                    )
-                );
-            }
+            $action = Request::input('action');
+            $info = $this->{$action}();
 
             return response()->json($info);
         }
+    }
+
+    protected function getBlanks() {
+        $forms = json_decode(Request::input('forms'));
+
+        return $this->dispatch(
+            new GetBlankPdfListCommand($forms)
+        );
+    }
+
+    protected function printBlanks() {
+        $forms = json_decode(Request::input('forms'));
+
+        return $this->dispatch(
+            new AssemblePdfCommand($forms)
+        );
+    }
+
+    protected function printFillForm() {
+        $student = json_decode(Request::input('student'));
+        $responses = json_decode(Request::input('responses'));
+
+        return $this->dispatch(
+            new FillPdfCommand($student, $responses)
+        );
     }
 
 }
