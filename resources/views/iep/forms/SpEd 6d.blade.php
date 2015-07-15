01f1a87d7e5e97cc538670a562c61adc2d6cff2d
wallyjaw <wallyjaw@gmail.com>
2015-07-15 08:27:02 -0600

    Updated sped 6d renderer with the newer pdf. Fixed a bug in Iep/Student where getting the last name would recall itself over and over and over...

--- resources/views/iep/forms/SpEd 6d.blade.php
+++ resources/views/iep/forms/SpEd 6d.blade.php
@@ -1,34 +1,30 @@
 <?php
 
-  $pdf->setField('your-district-school', config('iep.district.name'));
-  $pdf->setField('your-city', config('iep.district.city'));
-  $pdf->addStudent($student);
-  // ddd($responses);
+$pdf->setField('your-school-district', config('iep.district.name'));
+
+if (!empty($responses->get('student-requires-esy'))) {
+  $pdf->setField('student-requires-esy', 'On');
+}
+if (!empty($responses->get('student-does-not-require-esy'))) {
+  $pdf->setField('student-does-not-require-esy', 'On');
+}
+if (!empty($responses->get('esy-decision-to-be-documented'))) {
+  $pdf->setField('esy-decision-to-be-documented', 'On');
+}
+if (!empty($responses->get('notice-was-translated-orally'))) {
+  $pdf->setField('notice-was-translated-orally', 'On');
+}
+if (!empty($responses->get('parent-adult-verfy-understands'))) {
+  $pdf->setField('parent-adult-verfy-understands', 'On');
+}
+
 ?>
 
 @foreach ($responses->responses as $index => $response)
   @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'paragraph')
     @include('iep._partials.text')
-
-  {{-- Manually match responses here because the response values have commas which prevents regex-based parsing of the values --}}
-  @elseif ($response['field'] == 'participate')
-  <?php
-    $value = $response['value'];
-    if (strpos($value, 'Did not attend (document efforts to involve parent/adult student) OR') !== FALSE) {
-      $pdf->setField('participate:Did not attend (document efforts to involve parent/adult student) OR', 'On');
-    }
-    if (strpos($value, 'Participated via telephone, video conference or other means AND') !== FALSE) {
-      $pdf->setField('participate:Participated via telephone,video conference or other means AND', 'On');
-    }
-    if (strpos($value, 'Copy of this document was mailed to parent adult student on') !== FALSE) {
-      $pdf->setField('participate:Copy of this document was mailed to parent adult student on', 'On');
-    }
-  ?>
-  @elseif ($response['value'] == 'Student requires ESY services (Attach description of goals and services, amount, frequency and Form 6g)')
-    <?php $pdf->setField('student-requires-esy:Student requires ESY services (Attach description of goals and services, amount, frequency and Form 6g)', 'On') ?>
-    
   @elseif ($response['type'] == 'checkbox')
-    @include('iep._partials.checkbox', ['checked' => 'On', 'split' => '/,\s/'])
+    @include('iep._partials.checkbox', ['checked' => 'On', 'split' => '/,\s+/'])
   @elseif ($response['type'] == 'radio')
     @include('iep._partials.radio', ['checked' => 'On'])
   @endif
