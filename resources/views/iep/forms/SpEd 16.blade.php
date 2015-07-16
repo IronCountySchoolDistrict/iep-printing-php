e89bc760fa01e9db8286464eff25d7e693be0e1a
wallyjaw <wallyjaw@gmail.com>
2015-07-16 14:20:15 -0600

    Added renderer for sped 16

---
+++ resources/views/iep/forms/SpEd 16.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+$pdf->setField('your-school-district', config('iep.district.name'));
+$pdf->setField('your-city', config('iep.district.city'));
+$pdf->addStudent($student);
+
+?>
+
+@foreach ($responses->responses as $response)
+  @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'paragraph')
+    @if (strpos($response['field'], 'list-question') !== false)
+      <?php $pdf->setField($response['field'], $responses->get($response['field'] . '-check') .'. '.$response['value']) ?>
+    @else
+      @include('iep._partials.text')
+    @endif
+  @elseif ($response['type'] == 'checkbox')
+    @include('iep._partials.checkbox', ['checked' => 'On'])
+  @endif
+@endforeach
