9ff30d3621fb3a2ddea3114e770913db69bf52b4
wallyjaw <wallyjaw@gmail.com>
2015-07-16 08:13:41 -0600

    Updated 7a and 7b to use the latest pdf from the state for filling them out.

--- resources/views/iep/forms/SpEd 7a.blade.php
+++ resources/views/iep/forms/SpEd 7a.blade.php
@@ -1,16 +1,25 @@
 <?php
 
-$pdf->setField('your-district-school', config('iep.district.name'));
+$pdf->setField('your-district-school', config('iep.district.name'). '/' .$responses->get('school'));
 $pdf->setField('your-city', config('iep.district.city'));
+if ($responses->get('consent') == 'I DO') {
+  $pdf->setField('i-do-signature', $responses->get('consent-signature'));
+  $pdf->setField('i-do-date', $responses->get('consent-date'));
+}
+if ($responses->get('consent') == 'I DO NOT') {
+  $pdf->setField('i-do-not-signature', $responses->get('consent-signature'));
+  $pdf->setField('i-do-not-date', $responses->get('consent-date'));
+}
+
 
 ?>
 
 @foreach ($responses->responses as $index => $response)
-  @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'paragraph')
+  @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'paragraph' || $response['type'] == 'hidden')
     @include('iep._partials.text')
   @elseif ($response['type'] == 'checkbox')
-    @include('iep._partials.checkbox', ['checked' => 'On', 'split' => "/,\s/"])
+    @include('iep._partials.checkbox', ['split' => "/,\s+/"])
   @elseif ($response['type'] == 'radio'))
-    @include('iep._partials.radio', ['checked' => 'On'])
+    @include('iep._partials.radio', ['split' => "/,\s+/"])
   @endif
 @endforeach
--- resources/views/iep/forms/SpEd 7b.blade.php
+++ resources/views/iep/forms/SpEd 7b.blade.php
@@ -1,26 +1,24 @@
+<?php
 
+$pdf->setField('your-school-district', config('iep.district.name'));
+if (!empty($responses->get('upon-exiting-the-lea'))) {
+  $pdf->setField('upon-exiting-the-lea', 'Yes');
+}
+if (!empty($responses->get('notice-translated'))) {
+  $pdf->setField('notice-translated', 'Yes');
+}
+if (!empty($responses->get('adult-understands'))) {
+  $pdf->setField('adult-understands', 'Yes');
+}
+
+?>
 
 @foreach ($responses->responses as $response)
-  @if ($response['type'] == 'text' || $response['type'] == 'hidden')
+  @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'paragraph' || $response['type'] == 'hidden')
     @include('iep._partials.text')
   @elseif ($response['type'] == 'checkbox')
-    @if ($response['field'] == 'upon-exiting-the-lea')
-      <?php $pdf->fields['upon-exiting-the-lea'] = 'On'; ?>
-    @elseif (starts_with($response['field'], 'attach-copy'))
-      @if (str_contains($response['value'], '|'))
-        @include('iep._partials.checkbox', ['checked' => 'On'])
-      @else <?php
-        $values = explode(', ', $response['value']);
-        foreach ($values as $checkbox) {
-          $pdf->fields[$response['field'] . ':' . $checkbox] = 'On';
-        } ?>
-      @endif
-    @else
-      @include('iep._partials.checkbox', ['checked' => 'Yes'])
-    @endif
-  @elseif ($response['type'] == 'radio')
-    @include('iep._partials.radio')
+    @include('iep._partials.checkbox', ['split' => "/,\s+/"])
+  @elseif ($response['type'] == 'radio'))
+    @include('iep._partials.radio', ['split' => "/,\s+/"])
   @endif
 @endforeach
-
-@include('iep._partials.addStudent')
