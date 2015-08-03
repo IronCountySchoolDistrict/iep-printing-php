61e74c9bd038de1b34828fb8411eb01f0e9de08b
wallyjaw <wallyjaw@gmail.com>
2015-08-03 12:02:12 -0600

    added renderer for 6i. is an example of offshooting multiple pages.

---
+++ resources/views/iep/forms/SpEd 6i.blade.php
@@ -0,0 +1,40 @@
+<?php
+
+for ($i = 1; $i < 5; $i++) {
+  if (!empty($responses->get('team-members-name' . $i))) {
+    $forms[] = (object)[
+      'form' => (object)[ 'id' => "SpEd 6i - member $i", 'title' => "SpEd 6i" ],
+      'response' => [
+        (object)['field' => 'student', 'type' => 'text', 'response' => $student->getLastFirst()],
+        (object)['field' => 'scheduled-for', 'type' => 'text', 'response' => $responses->get('scheduled-for')],
+        (object)['field' => 'scheduled-on', 'type' => 'text', 'response' => $responses->get('scheduled-on')],
+        (object)['field' => 'location', 'type' => 'text', 'response' => $responses->get('location')],
+        (object)['field' => 'team-members-name', 'type' => 'text', 'response' => $responses->get("team-members-name$i")],
+        (object)['field' => 'role', 'type' => 'text', 'response' => $responses->get("role$i")],
+        (object)['field' => 'excused', 'type' => 'text', 'response' => $responses->get("reason$i") .'. '. $responses->get('excused')],
+        (object)['field' => 'adult-sign', 'type' => 'text', 'response' => $responses->get('adult-sign')],
+        (object)['field' => 'lea-sign', 'type' => 'text', 'response' => $responses->get('lea-sign')],
+        (object)['field' => 'adult-date', 'type' => 'text', 'response' => $responses->get('adult-date')],
+        (object)['field' => 'lea-date', 'type' => 'text', 'response' => $responses->get('lea-date')]
+      ]
+    ];
+  }
+}
+
+if (isset($forms)) {
+  foreach ($forms as $form) {
+    $files[] = Bus::dispatch(
+      new App\Commands\FillPdfCommand($student, [$form], $event->fileOption, $event->watermarkOption)
+    )['file'];
+  }
+
+  if (isset($files)) {
+    echo json_encode($files);
+  }
+} else {
+  $pdf->setField('your-school-district', config('iep.district.name'));
+  $pdf->setField('your-city', config('iep.district.city'));
+  foreach ($responses->responses as $response) {
+    ?> @include('iep._partials.text') <?php
+  }
+}
