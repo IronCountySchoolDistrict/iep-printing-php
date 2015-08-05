13a1aa05805a3fb9716827339ff49330523fd55b
wallyjaw <wallyjaw@gmail.com>
2015-08-05 12:43:56 -0600

    Renderer added for SpEd 6f1

---
+++ resources/views/iep/forms/SpEd 6f1.blade.php
@@ -0,0 +1,7 @@
+@foreach ($responses->responses as $response)
+  @if ($response['type'] == 'text' || $response['type'] == 'dropdown' || $response['type'] == 'hidden' || $response['type'] == 'paragraph')
+    @include('iep._partials.text')
+  @elseif ($response['type'] == 'checkbox' || $response['type'] == 'radio')
+    @include('iep._partials.checkbox', ['split' => '/,\s+/'])
+  @endif
+@endforeach
