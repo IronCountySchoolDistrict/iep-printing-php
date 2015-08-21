5c3c56323c8fc518ef8715bc6b57a4782588c6ee
Joseph Wallingford <wallyjaw@gmail.com>
2015-08-21 15:48:28 -0600

    Fixed bug in printing this form that it will work even when someone hasn't selected the amount of goals yet.

--- resources/views/iep/forms/SpEd 06bc.blade.php
+++ resources/views/iep/forms/SpEd 06bc.blade.php
@@ -100,6 +100,73 @@ if ($goals > 0) {
       ];
     }
   }
+} else {
+  $bForms[] = (object)[
+    'form' => (object)[
+      'id' => "SpEd 6b - goal 1",
+      'title' => 'SpEd 6b'
+    ],
+    'response' => [
+      (object)[ 'field' => 'student', 'type' => 'text', 'response' => $student->getLastFirst() ],
+      (object)[ 'field' => 'date-of-iep', 'type' => 'text', 'response' => $responses->get('date-of-iep') ],
+      (object)[ 'field' => 'correlate-with-transition-plan', 'type' => 'text', 'response' => $responses->get('correlate-with-transition-plan') ],
+      (object)[ 'field' => 'goal-number', 'type' => 'text', 'response' => 1 ],
+      (object)[ 'field' => 'describe-measurable-goal', 'type' => 'text', 'response' => $responses->get("goal1-description") ],
+      (object)[ 'field' => 'measured', 'type' => 'checkbox', 'response' => $responses->get("goal1-measured") ],
+      (object)[ 'field' => 'measured-other', 'type' => 'text', 'response' => $responses->get("goal1-measured-other") ],
+      (object)[ 'field' => 'report', 'type' => 'checkbox', 'response' => $responses->get("goal1-report") ],
+      (object)[ 'field' => 'report-other', 'type' => 'text', 'response' => $responses->get("goal1-report-other") ],
+      (object)[ 'field' => 'progress-date1', 'type' => 'text', 'response' => $responses->get("goal1-progress-date1") ],
+      (object)[ 'field' => 'progress-date2', 'type' => 'text', 'response' => $responses->get("goal1-progress-date2") ],
+      (object)[ 'field' => 'progress-date3', 'type' => 'text', 'response' => $responses->get("goal1-progress-date3") ],
+      (object)[ 'field' => 'progress-date4', 'type' => 'text', 'response' => $responses->get("goal1-progress-date4") ],
+      (object)[ 'field' => 'progress-code1', 'type' => 'text', 'response' => $responses->get("goal1-progress-code1") ],
+      (object)[ 'field' => 'progress-code2', 'type' => 'text', 'response' => $responses->get("goal1-progress-code2") ],
+      (object)[ 'field' => 'progress-code3', 'type' => 'text', 'response' => $responses->get("goal1-progress-code3") ],
+      (object)[ 'field' => 'progress-code4', 'type' => 'text', 'response' => $responses->get("goal1-progress-code4") ],
+      (object)[ 'field' => 'short-term-objectives', 'type' => 'text', 'response' => $responses->get("goal1-short-term-objectives") ]
+    ]
+  ];
+  $cForms[] = (object)[
+    'form' => (object)[
+      'id' => "SpEd 6c - goals 1 & 2",
+      'title' => 'SpEd 6c'
+    ],
+    'response' => [
+      (object)[ 'field' => 'student', 'type' => 'text', 'response' => $student->getLastFirst() ],
+      (object)[ 'field' => 'date-of-iep', 'type' => 'text', 'response' => $responses->get('date-of-iep') ],
+      (object)[ 'field' => 'goal1', 'type' => 'text', 'response' => 1 ],
+      (object)[ 'field' => 'goal1-description', 'type' => 'text', 'response' => $responses->get("goal1-description") ],
+      (object)[ 'field' => 'goal1-measured', 'type' => 'checkbox', 'response' => $responses->get("goal1-measured") ],
+      (object)[ 'field' => 'goal1-measured-other', 'type' => 'text', 'response' => $responses->get("goal1-measured-other") ],
+      (object)[ 'field' => 'goal1-report', 'type' => 'checkbox', 'response' => $responses->get("goal1-report") ],
+      (object)[ 'field' => 'goal1-report-other', 'type' => 'text', 'response' => $responses->get("goal1-report-other") ],
+      (object)[ 'field' => 'goal1-progress-date1', 'type' => 'text', 'response' => $responses->get("goal1-progress-date1") ],
+      (object)[ 'field' => 'goal1-progress-code1', 'type' => 'text', 'response' => $responses->get("goal1-progress-code1") ],
+      (object)[ 'field' => 'goal1-progress-date2', 'type' => 'text', 'response' => $responses->get("goal1-progress-date2") ],
+      (object)[ 'field' => 'goal1-progress-code2', 'type' => 'text', 'response' => $responses->get("goal1-progress-code2") ],
+      (object)[ 'field' => 'goal1-progress-date3', 'type' => 'text', 'response' => $responses->get("goal1-progress-date3") ],
+      (object)[ 'field' => 'goal1-progress-code3', 'type' => 'text', 'response' => $responses->get("goal1-progress-code3") ],
+      (object)[ 'field' => 'goal1-progress-date4', 'type' => 'text', 'response' => $responses->get("goal1-progress-date4") ],
+      (object)[ 'field' => 'goal1-progress-code4', 'type' => 'text', 'response' => $responses->get("goal1-progress-code4") ],
+      (object)[ 'field' => 'goal1-short-term-objectives', 'type' => 'text', 'response' => $responses->get("goal1-short-term-objectives") ],
+      (object)[ 'field' => 'goal2', 'type' => 'text', 'response' => 2 ],
+      (object)[ 'field' => 'goal2-description', 'type' => 'text', 'response' => $responses->get("goal2-description") ],
+      (object)[ 'field' => 'goal2-measured', 'type' => 'checkbox', 'response' => $responses->get("goal2-measured") ],
+      (object)[ 'field' => 'goal2-measured-other', 'type' => 'text', 'response' => $responses->get("goal2-measured-other") ],
+      (object)[ 'field' => 'goal2-report', 'type' => 'checkbox', 'response' => $responses->get("goal2-report") ],
+      (object)[ 'field' => 'goal2-report-other', 'type' => 'text', 'response' => $responses->get("goal2-report-other") ],
+      (object)[ 'field' => 'goal2-progress-date1', 'type' => 'text', 'response' => $responses->get("goal2-progress-date1") ],
+      (object)[ 'field' => 'goal2-progress-code1', 'type' => 'text', 'response' => $responses->get("goal2-progress-code1") ],
+      (object)[ 'field' => 'goal2-progress-date2', 'type' => 'text', 'response' => $responses->get("goal2-progress-date2") ],
+      (object)[ 'field' => 'goal2-progress-code2', 'type' => 'text', 'response' => $responses->get("goal2-progress-code2") ],
+      (object)[ 'field' => 'goal2-progress-date3', 'type' => 'text', 'response' => $responses->get("goal2-progress-date3") ],
+      (object)[ 'field' => 'goal2-progress-code3', 'type' => 'text', 'response' => $responses->get("goal2-progress-code3") ],
+      (object)[ 'field' => 'goal2-progress-date4', 'type' => 'text', 'response' => $responses->get("goal2-progress-date4") ],
+      (object)[ 'field' => 'goal2-progress-code4', 'type' => 'text', 'response' => $responses->get("goal2-progress-code4") ],
+      (object)[ 'field' => 'goal2-short-term-objectives', 'type' => 'text', 'response' => $responses->get("goal2-short-term-objectives") ]
+    ]
+  ];
 }
 
 foreach ($bForms as $form) {
