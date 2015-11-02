0dd458740d828d817c56a0bc0349c37a67054548
wallyjaw <wallyjaw@gmail.com>
2015-11-02 13:31:47 -0700

    updated 5k printing for new pdf

--- resources/views/iep/forms/SpEd 5k.blade.php
+++ resources/views/iep/forms/SpEd 5k.blade.php
@@ -1,1 +1,21 @@
-@include('iep.forms.SpEd 5e')
+<?php
+
+$pdf->setField('student', $student->getLastFirst());
+$pdf->setField('grade', $student->getGrade());
+$pdf->setField('dob', $student->getDob()->format('m/d/Y'));
+
+?>
+
+@foreach ($responses->responses as $response)
+	@if ($response['type'] == 'checkbox' || $response['type'] == 'radio')
+		@include('iep._partials.checkbox', ['split' => '/,\s+/'])
+	@else
+		@include('iep._partials.text')
+	@endif
+@endforeach
+
+<?php
+
+$pdf->setField('your-school-district', config('iep.district.name'));
+
+?>
