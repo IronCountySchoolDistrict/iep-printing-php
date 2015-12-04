3b139b89068204d1232c3aac46d35d326ffd42a5
= <wallyjaw@gmail.com>
2015-12-04 15:46:54 -0700

    added sped 6d to html printing

---
+++ resources/views/iep/html/headers/iep-sped-6d.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" charset="utf-8">
+        @include('iep.html._partials.header-script')
+    </head>
+    <body onload="header()">
+        <div class="row" id="header">
+            <div class="col-xs-7">
+                <span>{{ $student->get('lastfirst') }}</span>
+            </div>
+            <div class="col-xs-5 text-right">
+                <span>SpEd 6d 09.14</span>
+            </div>
+        </div>
+    </body>
+</html>
