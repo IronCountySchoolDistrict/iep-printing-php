ea9f1be7664d0bfff93190cba863d51a3f3be761
jonesnc <purkinje90@gmail.com>
2016-11-17 10:29:26 -0700

    Updated routes to laravel 5.3

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// originate from this app
 Route::get('/', 'FrameController@index');
 Route::get('/iep', 'FrameController@iep');
 Route::post('/iep', 'FrameController@save');
@@ -34,6 +33,6 @@ Route::any('/print-blanks', 'BaseController@printBlanks');
 Route::get('/token', 'BaseController@token');
 
 // learning center
-Route::get('/learning', function() {
-  return view('learning.index');
+Route::get('/learning', function () {
+    return view('learning.index');
 });
