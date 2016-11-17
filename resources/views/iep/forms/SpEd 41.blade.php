f5e0b424348ac9c99ebef00ec9194ef0255f476f
jonesnc <purkinje90@gmail.com>
2016-11-17 16:42:06 -0700

    Reformatting

--- resources/views/iep/forms/SpEd 41.blade.php
+++ resources/views/iep/forms/SpEd 41.blade.php
@@ -1,17 +1,15 @@
-<?php
-
-$pdf->setField('name', $student->getLastFirst());
-$pdf->setField('student-id', $student->student_number);
-$pdf->setField('dob', $student->getDob()->format('m/d/Y'));
-$pdf->setField('grade', $student->getGrade());
-$pdf->setField('school', $student->getSchoolName());
-
-?>
+@php
+    $pdf->setField('name', $student->getLastFirst());
+    $pdf->setField('student-id', $student->student_number);
+    $pdf->setField('dob', $student->getDob()->format('m/d/Y'));
+    $pdf->setField('grade', $student->getGrade());
+    $pdf->setField('school', $student->getSchoolName());
+@endphp
 
 @foreach ($responses->responses as $response)
-  @if ($response['type'] == 'checkbox' || $response['type'] == 'radio')
-    @include('iep._partials.checkbox', ['split' => '/,\s+/', 'checked' => 'On'])
-  @else
-    @include('iep._partials.text')
-  @endif
+    @if ($response['type'] == 'checkbox' || $response['type'] == 'radio')
+        @include('iep._partials.checkbox', ['checked' => 'On'])
+    @else
+        @include('iep._partials.text')
+    @endif
 @endforeach
--- resources/views/iep/forms/SpEd 44.blade.php
+++ resources/views/iep/forms/SpEd 44.blade.php
@@ -1,14 +1,13 @@
-<?php
-
-$pdf->setField('student', $student->getLastFirst());
-$pdf->setField('birthdate', $student->getDob()->format('m/d/Y'));
-
-?>
+@php
+    $pdf->setField('student', $student->getLastFirst());
+    $pdf->setField('birthdate', $student->getDob()->format('m/d/Y'));
+    $pdf->setField('grade', $student->getGrade());
+@endphp
 
 @foreach ($responses->responses as $response)
-  @if ($response['type'] == 'checkbox' || $response['type'] == 'radio')
-    @include('iep._partials.checkbox', ['split' => '/,\s+/', 'checked' => 'On'])
-  @else
-    @include('iep._partials.text')
-  @endif
+    @if ($response['type'] == 'checkbox' || $response['type'] == 'radio')
+        @include('iep._partials.checkbox', ['split' => '/,\s+/', 'checked' => 'On'])
+    @else
+        @include('iep._partials.text')
+    @endif
 @endforeach
