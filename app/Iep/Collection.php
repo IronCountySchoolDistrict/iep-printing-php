6c6f1354e2a5f5eb235ff21dc29684d0191e80a0
jonesnc <purkinje90@gmail.com>
2016-11-17 11:52:04 -0700

    Updated constructor and get() to account for when a response value is an array

--- app/Iep/Collection.php
+++ app/Iep/Collection.php
@@ -2,39 +2,57 @@
 
 namespace App\Iep;
 
-class Collection extends \Illuminate\Support\Collection {
-
-	public function __construct($items = []) {
-		if (is_array($items)) {
-			foreach ($items as $item) {
-				$this->items[$item->field] = [
-					'field' => $item->field,
-					'type' => $item->type,
-					'response' => $item->response
-				];
-			}
-		} else {
-			$this->items = $this->getArrayableItems($items);
-		}
-	}
-
-	/**
-	 * get function that returns 'response' field
-	 * @param  string  $key
-	 * @param  string  $default [description]
-	 * @param  boolean $flatten should the return value be a string or array?
-	 * @return string|array     return string value of response, or, if $flatten
-	 * is true, return the array as the value
-	 */
-	public function get($key, $flatten = False, $default = NULL) {
-		if ($this->offsetExists($key)) {
-			if ($flatten) {
-				return implode(', ', $this->items[$key]['response']);
-			} else {
-				return $this->items[$key]['response'];
-			}
+class Collection extends \Illuminate\Support\Collection
+{
+    /**
+     * Collection constructor.
+     * @param array $items
+     */
+    public function __construct($items = [])
+    {
+        if (is_array($items)) {
+            foreach ($items as $item) {
+                if (is_array($item)) {
+                    $this->items[$item['field']] = [
+                        'field' => $item['field'],
+                        'type' => $item['type'],
+                        'response' => $item['response'],
+                    ];
+                } else {
+                    $this->items[$item->field] = [
+                        'field' => $item->field,
+                        'type' => $item->type,
+                        'response' => $item->response,
+                    ];
+                }
+            }
+        } else {
+            $this->items = $this->getArrayableItems($items);
+        }
     }
 
-    return value($default);
-	}
+    /**
+     * get function that returns 'response' field.
+     *
+     * @param string $key
+     * @param string $default [description]
+     * @param bool $flatten if the element matching $key is an array, should the return value be a string or array?
+     *
+     * @return string|array return string value of response, or, if $flatten
+     *                      is true, return the array as the value
+     */
+    public function get($key, $flatten = false, $default = null)
+    {
+        if ($this->offsetExists($key)) {
+            if ($flatten && $this->items[$key]['response'] != null && is_array($this->items[$key]['response'])) {
+                $array_str = implode(', ', $this->items[$key]['response']);
+
+                return $array_str;
+            } else {
+                return $this->items[$key]['response'];
+            }
+        }
+
+        return value($default);
+    }
 }
