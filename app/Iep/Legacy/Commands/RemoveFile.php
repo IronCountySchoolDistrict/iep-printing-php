08ef7836d485e7bef5a2e3965fee20ed3dfa543b
jonesnc <purkinje90@gmail.com>
2016-11-17 11:48:47 -0700

    Update Job to use laravel 5.3's traits

--- app/Iep/Legacy/Commands/RemoveFile.php
+++ app/Iep/Legacy/Commands/RemoveFile.php
@@ -3,24 +3,27 @@
 namespace App\Iep\Legacy\Commands;
 
 use File;
-use App\Jobs\Job;
-use Illuminate\Queue\SerializesModels;
+use Exception;
+use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Bus\SelfHandling;
-use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
-class RemoveFile extends Job implements SelfHandling, ShouldBeQueued
+
+class RemoveFile implements ShouldQueue
 {
+    use InteractsWithQueue, Queueable, SerializesModels;
+
     public $files;
 
-    use InteractsWithQueue, SerializesModels;
     /**
      * Create a new job instance.
-     *
-     * @return void
+     * @param $files
      */
     public function __construct($files)
     {
+        Log::info('Creating RemoveFile command');
         $this->files = $files;
     }
 
@@ -34,13 +37,20 @@ class RemoveFile extends Job implements SelfHandling, ShouldBeQueued
         if ($this->attempts() < 3) {
             if (is_array($this->files)) {
                 foreach ($this->files as $file) {
+                    Log::info("Deleting file: " . public_path($file));
                     File::delete(public_path($file));
                 }
             } else {
-                File::delete(public_path($file));
+                Log::info("Deleting file: " . public_path($this->files));
+                File::delete(public_path($this->files));
             }
         } else {
             $this->delete();
         }
+    }
+
+    public function failed(Exception $exception)
+    {
+        Log::info($exception);
     }
 }
