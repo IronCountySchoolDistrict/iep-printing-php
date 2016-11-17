d200922a7d1a8d30bbfb4553f8cd67338a5efd68
jonesnc <purkinje90@gmail.com>
2016-11-17 10:51:31 -0700

    Formatting

--- resources/views/frame/modal/delete.blade.php
+++ resources/views/frame/modal/delete.blade.php
@@ -1,25 +1,26 @@
 <div class="modal fade delete-iep-modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <h4 class="modal-title">Delete Iep</h4>
-      </div>
-      <div class="modal-body">
-        <div class="form">
-          <div class="form-group">
-            <label>Type DELETE to confirm</label>
-            <input class="form-control" type="text" name="confirm-delete" autocomplete="off">
-            <p class="help-block"></p>
-          </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Delete Iep</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form">
+                    <div class="form-group">
+                        <label>Type DELETE to confirm</label>
+                        <input class="form-control" type="text" name="confirm-delete" autocomplete="off">
+                        <p class="help-block"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger"><i class="fa fa-spinner fa-pulse fa-fw hide"></i> Delete
+                </button>
+            </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-danger"><i class="fa fa-spinner fa-pulse fa-fw hide"></i> Delete</button>
-      </div>
     </div>
-  </div>
 </div>
--- resources/views/iep/layouts/default.blade.php
+++ resources/views/iep/layouts/default.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>
-            @yield('title')
-        </title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" charset="utf-8">
-        @yield('stylesheet')
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>
+        @yield('title')
+    </title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" charset="utf-8">
+    @yield('stylesheet')
+</head>
+<body>
+@yield('content')
+</body>
 </html>
